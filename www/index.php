983eb41291f16e3affc4c3c5a492a41c5530c223
Loic Hamou <loic.hamou@free.fr>
2019-09-22 21:58:46 +0200

    WOrking

--- www/index.php
+++ www/index.php
@@ -1,11 +1,36 @@
 <?php
 
+
+
 use TheCodingMachine\Gotenberg\Client;
 use TheCodingMachine\Gotenberg\DocumentFactory;
 use TheCodingMachine\Gotenberg\HTMLRequest;
 
-$client = new Client('http://localhost:3000', new \Http\Adapter\Guzzle6\Client());
-$index = DocumentFactory::makeFromPath('index.html', 'index.html');
+chdir(dirname(__file__));
+
+echo getcwd() . "\n";
+echo '';
+echo '<pre>';
+print_r(scandir(dirname(__file__)));
+echo '</pre>';
+
+function chmod_r($path) {
+	$dir = new DirectoryIterator($path);
+	foreach ($dir as $item) {
+		chmod($item->getPathname(), 0777);
+		if ($item->isDir() && !$item->isDot()) {
+			chmod_r($item->getPathname());
+		}
+	}
+}
+
+chmod_r('pdf');
+echo exec('whoami');
+
+
+require 'vendor/autoload.php';
+
+$client = new Client('http://dockertp_gotenberg_1:3000', new \Http\Adapter\Guzzle6\Client());
+$index = DocumentFactory::makeFromPath('index.html', 'pdf/');
 $request = new HTMLRequest($index);
-$dirPath = "/foo";
-$filename = $client->store($request, $dirPath);
+$filename = $client->store($request, 'pdf/test.pdf');
