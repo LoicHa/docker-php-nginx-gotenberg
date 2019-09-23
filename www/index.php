08c9bd3e2db6fdc29ae91a0f970e7185e99de1d2
Loic Hamou <loic.hamou@free.fr>
2019-09-23 09:51:26 +0200

    Setup ok

--- www/index.php
+++ www/index.php
@@ -6,31 +6,15 @@ use TheCodingMachine\Gotenberg\Client;
 use TheCodingMachine\Gotenberg\DocumentFactory;
 use TheCodingMachine\Gotenberg\HTMLRequest;
 
-chdir(dirname(__file__));
-
-echo getcwd() . "\n";
-echo '';
-echo '<pre>';
-print_r(scandir(dirname(__file__)));
-echo '</pre>';
-
-function chmod_r($path) {
-	$dir = new DirectoryIterator($path);
-	foreach ($dir as $item) {
-		chmod($item->getPathname(), 0777);
-		if ($item->isDir() && !$item->isDot()) {
-			chmod_r($item->getPathname());
-		}
-	}
-}
-
-chmod_r('pdf');
-echo exec('whoami');
-
 
 require 'vendor/autoload.php';
 
-$client = new Client('http://dockertp_gotenberg_1:3000', new \Http\Adapter\Guzzle6\Client());
-$index = DocumentFactory::makeFromPath('index.html', 'pdf/');
+ob_start();
+require('pdf.php');
+$content = ob_get_clean();
+
+
+$client = new Client('http://dockertp_gotenberg_1:4000', new \Http\Adapter\Guzzle6\Client());
+$index = DocumentFactory::makeFromString('index.html', $content);
 $request = new HTMLRequest($index);
 $filename = $client->store($request, 'pdf/test.pdf');
--- www/pdf.php
+++ www/pdf.php
@@ -5,6 +5,8 @@
 		<title>My PDF</title>
 	</head>
 	<body>
-		<h1>Hello world!</h1>
+		<h1><?php echo 'ok' ?></h1>
+		<div style="page-break-after:always"></div>
+		<h1><?php echo 'ok2' ?></h1>
 	</body>
 </html>
